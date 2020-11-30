28053b816b046588a6a7b612d32243df0bf32d03
Sergio <38168163+FundacionBlockchainChile@users.noreply.github.com>
2020-11-29 22:03:16 -0300

    Testing to Component - Container - Redux  added

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
-// import Auth from './containers/Auth/Auth';
-// import CheckOut from './containers/Checkout/Checkout';
-// import Orders from './containers/Orders/Orders';
 
 const asyncCheckout = asyncComponent(() => {
   return import('./containers/Checkout/Checkout');
@@ -24,12 +21,11 @@ const asyncAuth = asyncComponent(() => {
 });
 
 class App extends Component {
-  componentDidMount() {
+  componentDidMount () {
     this.props.onTryAutoSignup();
   }
 
-  render() {
-    // Route protection in FrontEnd
+  render () {
     let routes = (
       <Switch>
         <Route path="/auth" component={asyncAuth} />
@@ -38,7 +34,7 @@ class App extends Component {
       </Switch>
     );
 
-    if (this.props.isAuthenticated) {
+    if ( this.props.isAuthenticated ) {
       routes = (
         <Switch>
           <Route path="/checkout" component={asyncCheckout} />
@@ -53,22 +49,24 @@ class App extends Component {
 
     return (
       <div>
-        <Layout>{routes}</Layout>
+        <Layout>
+          {routes}
+        </Layout>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
-    isAuthenticated: state.auth.token !== null,
+    isAuthenticated: state.auth.token !== null
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    onTryAutoSignup: () => dispatch(actions.authCheckState()),
+    onTryAutoSignup: () => dispatch( actions.authCheckState() )
   };
 };
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+export default withRouter( connect( mapStateToProps, mapDispatchToProps )( App ) );
